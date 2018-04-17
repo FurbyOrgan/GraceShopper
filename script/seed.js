35431916fccf9dc14b569435ab970e9ce9efadce
Victor Barrancos <victorbarrancos@gmail.com>
2018-04-17 14:05:55 -0500

    Seed file partially done.  Fixed some assocations

--- script/seed.js
+++ script/seed.js
@@ -9,23 +9,91 @@
  *
  * Now that you've got the main idea, check it out in practice below!
  */
-const db = require('../server/db')
-const {User} = require('../server/db/models')
+const db = require('../server/db');
+const {
+  Address,
+  CartItem,
+  Category,
+  LineItem,
+  Order,
+  Product,
+  Review,
+  User
+} = require('../server/db/models');
 
-async function seed () {
-  await db.sync({force: true})
-  console.log('db synced!')
+async function seed() {
+  await db.sync({ force: true });
+  console.log('db synced!');
   // Whoa! Because we `await` the promise that db.sync returns, the next line will not be
   // executed until that promise resolves!
 
   const users = await Promise.all([
-    User.create({email: 'cody@email.com', password: '123'}),
-    User.create({email: 'murphy@email.com', password: '123'})
-  ])
-  // Wowzers! We can even `await` on the right-hand side of the assignment operator
-  // and store the result that the promise resolves to in a variable! This is nice!
-  console.log(`seeded ${users.length} users`)
-  console.log(`seeded successfully`)
+    User.create({
+      firstName: 'John',
+      lastName: 'Doe',
+      email: 'john@email.com',
+      isAdmin: false,
+      password: '123'
+    }),
+    User.create({
+      firstName: 'Jane',
+      lastName: 'Doe',
+      email: 'jane@email.com',
+      isAdmin: true,
+      password: '123'
+    })
+  ]);
+
+  const categories = await Promise.all([
+    Category.create({
+      name: 'Copy Paper',
+      description: "It's just paper."
+    }),
+    Category.create({
+      name: 'Sticky Notes',
+      description: 'Leave passive-aggressive notes around the office.'
+    }),
+    Category.create({ name: 'Lined Paper', description: 'Paper: now with lines.' }),
+    Category.create({ name: 'Construction Paper', description: 'Make your own South Park episode!' })
+  ]);
+
+  const products = await Promise.all([
+    Product.create({
+      title: 'Yellow Sticky Note',
+      description: "It's a single yellow sticky note.",
+      price: 39.99,
+      inventory: 500
+    }),
+    Product.create({
+      title: 'Letter Copy Paper',
+      description: 'One sheet of 8.5"x11" copy paper.',
+      price: 199.99,
+      inventory: 2000
+    }),
+    Product.create({
+      title: 'Lined Paper',
+      description: 'A single sheet of college-ruled lined paper.',
+      price: 59.99,
+      inventory: 3
+    })
+  ]);
+
+  await products[0].addCategory(categories[1]);
+  await products[1].addCategory(categories[0]);
+  await products[2].addCategory(categories[2]);
+
+  console.log('Building review');
+  const review = Review.build({
+    body: 'This paper arrived torn in half and scribbled with blue crayon.  Very dissatisfied.',
+    rating: 2
+  });
+  console.log('Setting associations');
+  review.setUser(users[0]);
+  review.setProduct(products[1]);
+  console.log('Review:', review);
+  await review.save();
+
+  console.log(`seeded successfully`);
 }
 
 // Execute the `seed` function
@@ -33,19 +101,19 @@ async function seed () {
 // that might occur inside of `seed`
 seed()
   .catch(err => {
-    console.error(err.message)
-    console.error(err.stack)
-    process.exitCode = 1
+    console.error(err.message);
+    console.error(err.stack);
+    process.exitCode = 1;
   })
   .then(() => {
-    console.log('closing db connection')
-    db.close()
-    console.log('db connection closed')
-  })
+    console.log('closing db connection');
+    db.close();
+    console.log('db connection closed');
+  });
 
 /*
  * note: everything outside of the async function is totally synchronous
  * The console.log below will occur before any of the logs that occur inside
  * of the async function
  */
-console.log('seeding...')
+console.log('seeding...');
