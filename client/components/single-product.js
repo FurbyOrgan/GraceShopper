0fd60fb38d6f365fee09b0380dc973f6bdda239a
Lisa Lam <lisalam@Lisas-MacBook.local>
2018-04-17 15:42:06 -0500

    idiotically simple single product page

---
+++ client/components/single-product.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function SingleProduct(props) {
+    const product = props;
+    return (
+        <div>
+            <h3>{product.name}</h3>
+            <h4>{product.price}</h4>
+            <h6>{product.inventory}</h6>
+            <p>{product.desc}</p>
+            <ul>
+                <li>{product.categories}</li>
+            </ul>
+        </div>
+    );
+}
