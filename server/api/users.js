cf179e68a6e66c64b1e57e8bee0b60914a6ee311
Vi Tran <vipran@gmail.com>
2018-04-17 13:47:51 -0500

    added main user routes

--- server/api/users.js
+++ server/api/users.js
@@ -2,13 +2,51 @@ const router = require('express').Router()
 const {User} = require('../db/models')
 module.exports = router
 
-router.get('/', (req, res, next) => {
-  User.findAll({
-    // explicitly select only the id and email fields - even though
-    // users' passwords are encrypted, it won't help if we just
-    // send everything to anyone who asks!
-    attributes: ['id', 'email']
+
+router.param('id', (req, res, next) => {
+  User.findById(id)
+  .then(user => {
+    if(!user) throw "Cannot find User"
+    req.requestedUser = user;
+    next()
   })
+  .catch(next)
+})
+
+
+
+router.get('/', (req, res, next) => {
+  User.findAll()
     .then(users => res.json(users))
     .catch(next)
+})
+
+router.post('/', (req, res, next) => {
+  User.create(req.body)
+  .then(user => res.status(201)).json(user)
+  .catch(next);
+
+
+})
+
+router.get('/:id', (req, res, next) =>{
+  req.requestedUser.reload()
+  .then(requestedUser => res.json(requestedUser))
+  .catch(next);
+
+})
+
+
+router.put('/:id', (req, res, next) =>{
+  req.requestedUser.update(req.body)
+  .then(user => res.json(user))
+  .catch(next)
+})
+
+router.delete('/:id', (req, res, next) =>{
+  req.requestedUser.destroy()
+  .then( () => {
+    res.sendStatus(204)
+  })
+  .catch(next)
 })
