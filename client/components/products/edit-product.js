f5b6dfb96e60103d7b9dc0061def2667f324cda6
livilammister <livilammister@gmail.com>
2018-04-19 11:22:55 -0500

    forgot some fields on the form

--- client/components/products/edit-product.js
+++ client/components/products/edit-product.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Checkbox, Form, Input } from 'semantic-ui-react';
+import { Button, Checkbox, Container, Form, Input, TextArea } from 'semantic-ui-react';
 
 /**
  * v1 - render something
@@ -10,18 +10,25 @@ export default class EditProduct extends Component {
 
     render() {
         return (
-            <Form>
-                <Form.Group widths="equal">
-                    <Form.Field control={Input} label="title" placeholder="Product Title" />
-                    <Form.Field control={Input} label="price" placeholder="Product Price" />
-                    <Form.Field control={Input} label="inventory" placeholder="Product Inventory" />
-                </Form.Group>
-                <Form.Group inline>
-                    <label>Categories</label>
-                    <Form.Field control={Checkbox} label='Category 1' />
-                    <Form.Field control={Checkbox} label='Category 2' />
-                </Form.Group>
-            </Form>
+            <Container text>
+                <Form>
+                    <Form.Group>
+                        <Form.Field control={Input} label="Title" placeholder="Product Title" />
+                        <Form.Field control={Input} label="Price" placeholder="Product Price" />
+                        <Form.Field control={Input} label="Inventory" placeholder="Product Inventory" />
+                    </Form.Group>
+                    <Form.Group inline>
+                        <label>Categories</label>
+                        <Form.Field control={Checkbox} label='Category 1' />
+                        <Form.Field control={Checkbox} label='Category 2' />
+                    </Form.Group>
+                    <Form.Field control={TextArea} label="Description" placeholder="Product Description" />
+                    <Form.Group inline>
+                        <Form.Field id='form-button-control-public' control={Button} content='Confirm' />
+                        <Form.Field id='form-button-control-public' control={Button} content='Cancel' />
+                    </Form.Group>
+                </Form>
+            </Container>
         )
     }
 }
