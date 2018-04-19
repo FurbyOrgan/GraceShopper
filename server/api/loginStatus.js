cb8dbdadaa2c96cb50c97fc3e738f562059e7e2a
rawarne2 <rashaunwarner@yahoo.com>
2018-04-19 10:11:14 -0500

    middleware functions

---
+++ server/api/loginStatus.js
@@ -0,0 +1,28 @@
+function isLoggedIn(req, res, next) {
+    if (req.user.id === req.params.id)
+        return next();
+    res.redirect('/')
+}
+
+
+function isAdmin(req, res, next) {
+    if (req.user && req.user.isAdmin){
+        return next();
+    }
+    else {
+        throw new Error('Not an admin')
+    }
+}
+
+
+function isSelfOrAdmin(req, res, next) {
+    if (req.params.id == req.user.id || req.user.isAdmin)
+        return next();
+    res.redirect('/')
+}
+
+module.exports = {
+    isLoggedIn,
+    isAdmin,
+    isSelfOrAdmin
+}
