50221c870043382a57336740fe3750d87936e34f
Victor Barrancos <victorbarrancos@gmail.com>
2018-04-19 10:46:12 -0500

    Cart API enhancements, started cart store persistence

--- client/store/cart.js
+++ client/store/cart.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import history from '../history';
+import store from '../store';
 
 // Initial State
 const initialCartState = [];
@@ -7,8 +8,6 @@ const initialCartState = [];
 // Action Types
 const LOAD_CART = 'LOAD_CART';
 const UPDATE_QUANTITY = 'UPDATE_QUANTITY';
-
-// Action Creators
 
 // Thunk Creators
 
@@ -20,34 +19,43 @@ export const refreshCart = () => {
       .catch(err => console.log(err));
 };
 
-export const updateQuantity = (productId, quantity) => ({
-  type: UPDATE_QUANTITY,
-  payload: { productId, quantity }
-});
+// If the user is logged in, updateQuantity will POST to the API with the change to their cart.
+// If they are not logged in, updateQuantity will instead update window.localStorage.
+// In both cases, the cart store in Redux will be updated.
+export const updateQuantity = (productId, quantity) => {
+  return (dispatch, getState) => {
+    const updateQuantityAction = {
+      type: UPDATE_QUANTITY,
+      payload: { productId, quantity }
+    };
+    dispatch(updateQuantityAction);
+    if (getState().user.id) {axios.post('/api/cart', updateQuantityAction.payload)}
+    else {window.localStorage.setItem('guestCart', JSON.stringify(getState().cart))}
+  }
+};
 
 export default function reducer(cart = initialCartState, action) {
   switch (action.type) {
     case LOAD_CART:
       return action.payload;
-    case UPDATE_QUANTITY:
-      {
-        const item = action.payload;
-        // If quantity is zero, remove the item from the cart.
-        if (item.quantity === 0) return cart.filter(element => element.productId !== item.productId);
+    case UPDATE_QUANTITY: {
+      const item = action.payload;
+      // If quantity is zero, remove the item from the cart.
+      if (item.quantity === 0) return cart.filter(element => element.productId !== item.productId);
 
-        // Otherwise, update the cart quantity, provided that the product exists in the cart.
-        let isNewItem = true;
-        const updatedCart = cart.map(element => {
-          if (element.productId === item.productId) {
-            isNewItem = false;
-            return item;
-          }
-          return element;
-        });
+      // Otherwise, update the cart quantity, provided that the product exists in the cart.
+      let isNewItem = true;
+      const updatedCart = cart.map(element => {
+        if (element.productId === item.productId) {
+          isNewItem = false;
+          return item;
+        }
+        return element;
+      });
 
-        // If the product doesn't exist in the cart, add it.
-        return isNewItem ? [...cart, item] : updatedCart;
-      }
+      // If the product doesn't exist in the cart, add it.
+      return isNewItem ? [...cart, item] : updatedCart;
+    }
     default:
       return cart;
   }
--- server/api/cart.js
+++ server/api/cart.js
@@ -20,13 +20,21 @@ router.get('/', async (req, res, next) => {
 
 // Add a new item to the user's shopping cart.
 // If the item already exists, the quantity will be updated.
+// If the quantity is zero, the item will be removed from the cart.
 router.post('/', async (req, res, next) => {
   try {
-    const cartItem = (await CartItem.findOrCreate({
-      where: { userId: req.user.id, productId: req.body.productId }
-    }))[0];
-    cartItem.quantity = req.body.quantity;
-    res.json(await cartItem.save());
+    if (req.body.quantity === 0) {
+      const result = await CartItem.destroy({
+        where: { userId: req.user.id, productId: req.body.productId }
+      });
+      res.sendStatus(204);
+    } else {
+      const cartItem = (await CartItem.findOrCreate({
+        where: { userId: req.user.id, productId: req.body.productId }
+      }))[0];
+      cartItem.quantity = req.body.quantity;
+      res.json(await cartItem.save());
+    }
   } catch (error) {
     next(error);
   }
