33ddc1b0327ef1cd07ef9c16a9b995997690bc29
Victor Barrancos <victorbarrancos@gmail.com>
2018-04-20 12:36:12 -0500

    Fixed syntax error and empty localStorage initialization

--- client/components/cart/cart-navbar-button.js
+++ client/components/cart/cart-navbar-button.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
-import { Button, Icon, Label } from 'semantic-ui-react'
+import { Button, Icon, Label } from 'semantic-ui-react';
 
 const CartNavbarButton = props => {
-  const itemsInCart = props.cart.reduce((total, currentElement) => total += currentElement.quantity, 0);
-  return <div className="cart"><Button className="ui teal labeled icon button" size="tiny"><i className="cart icon"></i> My Cart ({`${itemsInCart} item${itemsInCart !== 1 ? 's' : ''}`})</Button></div>;
-
+  const itemsInCart = props.cart.reduce((total, currentElement) => (total += currentElement.quantity), 0);
+  return (
+    <div className="cart">
+      <Button className="ui teal labeled icon button" size="tiny">
+        <i className="cart icon" /> My Cart ({`${itemsInCart} item${itemsInCart !== 1 ? 's' : ''}`})
+      </Button>
+    </div>
+  );
+};
 
 const mapState = state => ({
   products: state.products,
