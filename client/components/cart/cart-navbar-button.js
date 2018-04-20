0ef56f5168333421386d8ea5535c3ac9939688c4
Vi Tran <vipran@gmail.com>
2018-04-19 23:10:36 -0500

    fix button

--- client/components/cart/cart-navbar-button.js
+++ client/components/cart/cart-navbar-button.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Button, Icon, Label } from 'semantic-ui-react'
 
 const CartNavbarButton = props => {
   const itemsInCart = props.cart.reduce((total, currentElement) => total += currentElement.quantity, 0);
-  return <button className="ui teal labeled icon button"><i className="cart icon"></i> My Cart ({`${itemsInCart} item${itemsInCart !== 1 ? 's' : ''}`})</button>;
+  return <div className="cart"><Button className="ui teal labeled icon button" size="tiny"><i className="cart icon"></i> My Cart ({`${itemsInCart} item${itemsInCart !== 1 ? 's' : ''}`})</Button></div>;
 };
 
 const mapState = state => ({
