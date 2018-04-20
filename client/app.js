a0a818a7c3acbba2cfff7d713f0e76ac8a09ed5d
livilammister <livilammister@gmail.com>
2018-04-20 14:10:37 -0500

    put all the routes in routes.js

--- client/app.js
+++ client/app.js
@@ -2,16 +2,11 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { refreshProductList, refreshCategoryList, restoreCart } from './store';
 import { Navbar } from './components';
-import { ReviewForm} from './components'
+import { ReviewForm } from './components'
 import { Route, withRouter } from 'react-router-dom'
 
 import Routes from './routes';
-import CartList from './components/cart/cart-list';
-import CartNavbarButton from './components/cart/cart-navbar-button';
-import ProductList from './components/products/product-list';
-import SingleProduct from './components/products/single-product';
-import DummyHome from './components/dummyhome'
-import ProductReviews from './components/reviews/product-reviews'
+
 //import UserReviews from './components/reviews/user-reviews'
 
 class App extends React.Component {
@@ -24,10 +19,6 @@ class App extends React.Component {
       <div>
         <Navbar />
         <Routes />
-        <Route exact path="/" component={DummyHome} />
-        <Route exact path="/products" component={ProductList} />
-        <Route exact path="/products/:id" component={SingleProduct} />
-        <Route exact path="/review" component={ReviewForm} />
       </div>
     );
   }
--- client/routes.js
+++ client/routes.js
@@ -2,12 +2,18 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { withRouter, Route, Switch } from 'react-router-dom';
 import PropTypes from 'prop-types';
-import { Login, Signup, UserHome, ProductList, SearchResults } from './components';
-import CartList from './components/cart/cart-list'
-import CategoryList from './components/categories/category-list';
-import CategoryProductList from './components/categories/category-product-list';
-import UserReviews from './components/reviews/user-reviews'
-import AssignCategories from './components/categories/assign-categories'
+
+import { AssignCategories,
+         CartList, 
+         CategoryList,
+         CategoryProductList, 
+         Login,
+         ProductList,
+         Signup,
+         SearchResults,
+         UserHome,
+         UserReviews } from './components';
+
 import { me } from './store';
 
 /**
@@ -24,12 +30,14 @@ class Routes extends Component {
     return (
       <Switch>
         {/* Routes placed here are available to all visitors */}
-        <Route path="/cart" component={CartList} />
-        <Route path="/categories/:categoryId" component={CategoryProductList} />
-        <Route path="/categories" component={CategoryList} />
+        <Route exact path="/cart" component={CartList} />
+        <Route exact path="/products" component={ProductList} />
+        <Route exact path="/categories/:categoryId" component={CategoryProductList} />
+        <Route exact path="/categories" component={CategoryList} />
         <Route path="/search/:searchQuery" component={SearchResults} />
-        <Route path="/login" component={Login} />
-        <Route path="/signup" component={Signup} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/signup" component={Signup} />
+
         {isLoggedIn && (
           <Switch>
             {/* Routes placed here are only available after logging in */}
